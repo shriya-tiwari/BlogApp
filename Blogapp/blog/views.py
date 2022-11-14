ff4f65503f79786f1b78f36865e5619452d6509e
Maithri Muralikrishna <m.muralikrishna.cse21@itbhu.ac.in>
2022-11-14 23:20:47 +0530

    Added database

--- Blogapp/blog/views.py
+++ Blogapp/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Shriya',
-        'title': 'Blog post',
-        'content': 'First post conten',
-        'date-posted': 'November 14th, 2022'
-    },
-    {
-        'author': 'Maithri',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date-posted': 'November 15th, 2022'
-    },
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 

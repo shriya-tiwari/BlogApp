cb4954bc7562bf68750cc30c7a93dc150bb55d6c
Maithri Muralikrishna <m.muralikrishna.cse21@itbhu.ac.in>
2022-11-15 00:49:17 +0530

    Added User Registration

--- Blogapp/Blogapp/urls.py
+++ Blogapp/Blogapp/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls'),)
+    path('register/', user_views.register, name='register'),
+    path('blog/', include('blog.urls'),),
+
 ]
